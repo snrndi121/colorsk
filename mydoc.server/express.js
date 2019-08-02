01ffc4abfa47a5386a3f26796307d5c208f882c5
snrndi121 <snrndi4521@gmail.com>
2019-08-02 17:42:32 +0900

    server : syman_1_test11

--- mydoc.server/express.js
+++ mydoc.server/express.js
@@ -176,64 +176,19 @@ app.post('/', function (req, res) {
   };
   res.json(body);
 })
-app.post('/MC.ACTION.match', function (req, res){
-  console.log("\n>> APi_match from SK match");
-  var action_name = req.body.action.actionName;
-  var mVersion = req.body.version;
-  var mAction = req.body.action;
-  var mParams = req.body.action.parameters;
-  var mresultCode = 'OK';
-  console.log(mAction);
-  console.log(mParams);
-  IsMatchColor(mParams.src_color, mParams.dst_color)
-  .then(function(match_res) {
-          console.log("IsMatchColor");
-          var body = {
-            version : mVersion,
-            resultCode : mresultCode,
-            output : {
-              src_cloth : mParams.src_cloth,//action_params.requestNum,
-              src_color : mParams.src_color,
-              dst_cloth : mParams.dst_cloth,
-              dst_color : mParams.dst_color,
-              query_type : mParams.query_type,
-              resultCode : mresultCode,
-              resultColor : match_res
-            },
-            directives : []
-          };
-    })
-    .then(function() {
-        console.log(">> last call");
-        console.log(body);
-        res.json(body);
-    })
-})
 // app.post('/MC.ACTION.match', function (req, res){
+//   console.log("\n>> APi_match from SK match");
+//   var act// app.post('/MC.ACTION.match', function (req, res){
 //   console.log("\n>> APi_match from SK match");
 //   var action_name = req.body.action.actionName;
 //   var mVersion = req.body.version;
 //   var mAction = req.body.action;
 //   var mParams = req.body.action.parameters;
-//   var mresultColor = "";
 //   var mresultCode = 'OK';
 //   console.log(mAction);
 //   console.log(mParams);
-//   var src_id, dst_id;
-//   getColorCategory(mParams.src_color.value)
-//   .then(function(id) {
-//         src_id = id;
-//         console.log("\n >>(1) find the source_id : " + src_id);})
-//   .catch(function(err) {
-//       console.log("\n >> (1)sorry, server cannot find the source color");})
-//   .then(getColorCategory(mParams.dst_color.value)
-//       .then(function(id) {
-//           dst_id = id;
-//           console.log("\n >> (2)find the dest_id : " + dst_id);})
-//       .catch(function(err) {
-//           console.log("\n >>(2) sorry, server cannot find the dest color");})
-//   .then(IsMatchColor(mParams.src_color, mParams.dst_color)
-//       .then(function(match_res) {
+//   IsMatchColor(mParams.src_color, mParams.dst_color)
+//   .then(function(match_res) {
 //           console.log("IsMatchColor");
 //           var body = {
 //             version : mVersion,
@@ -251,73 +206,134 @@ app.post('/MC.ACTION.match', function (req, res){
 //           };
 //     })
 //     .then(function() {
-//         console.log("3");
+//         console.log(">> last call");
+//         console.log(body);
 //         res.json(body);
 //     })
-// ).then(function() {
-//     console.log(">> all done");
+// })ion_name = req.body.action.actionName;
+//   var mVersion = req.body.version;
+//   var mAction = req.body.action;
+//   var mParams = req.body.action.parameters;
+//   var mresultCode = 'OK';
+//   console.log(mAction);
+//   console.log(mParams);
+//   IsMatchColor(mParams.src_color, mParams.dst_color)
+//   .then(function(match_res) {
+//           console.log("IsMatchColor");
+//           var body = {
+//             version : mVersion,
+//             resultCode : mresultCode,
+//             output : {
+//               src_cloth : mParams.src_cloth,//action_params.requestNum,
+//               src_color : mParams.src_color,
+//               dst_cloth : mParams.dst_cloth,
+//               dst_color : mParams.dst_color,
+//               query_type : mParams.query_type,
+//               resultCode : mresultCode,
+//               resultColor : match_res
+//             },
+//             directives : []
+//           };
+//     })
+//     .then(function() {
+//         console.log(">> last call");
+//         console.log(body);
+//         res.json(body);
+//     })
 // })
+app.post('/MC.ACTION.match', function (req, res){
+  console.log("\n>> APi_match from SK match");
+  var action_name = req.body.action.actionName;
+  var mVersion = req.body.version;
+  var mAction = req.body.action;
+  var mParams = req.body.action.parameters;
+  var mresultColor = "";
+  var mresultCode = 'OK';
+  console.log(mAction);
+  console.log(mParams);
+  var src_id, dst_id;
+  var body = {
+    version : mVersion,
+    resultCode : mresultCode,
+    output : {
+      src_cloth : mParams.src_cloth,
+      src_color : mParams.src_color,
+      dst_cloth : mParams.dst_cloth,
+      dst_color : mParams.dst_color,
+      query_type : mParams.query_type,
+      resultCode : mresultCode,
+      resultColor : "err_undef_color"
+    },
+    directives : []
+  };
+  //get color code from request 'src_color', 'dst_color'
+  getColorCategory(mParams.src_color.value)
+  .then( function(src_id) {
+        console.log("\n >>(1) find the source_id : " + src_id);
+        getColorCategory(mParams.dst_color.value)
+        .then( function(dst_id) {
+            console.log("\n >> (2)find the dest_id : " + dst_id);
+            IsMatchColor(src_id, dst_id).then( function(match_res) {
+                body.resultColor = match_res;
+                console.log("\nres\n");
+                console.log(body);
+                return res.json(body);
+            })
+          })
+        .catch(function(err) {
+            console.log("\n >> (2)sorry, server cannot find the source color");
+            return res.json(body);
+        })
+  })
+  .catch(function(err) {
+      console.log("\n >> (1)sorry, server cannot find the source color");
+      return res.json(body);
+  })
+})
 /*
   *
   ******* part4.서버-함수 선언 *******
   *
 */
 //4.1 두 컬러간 매칭 파악
-function IsMatchColor(src_param, dst_param) {
+function IsMatchColor(src_color_id, dst_color_id) {
     return new Promise(function (resolve, reject) {
-        var res = {src_id : '', dst_id : ''};
         //exception
-        if (!src_param) {
+        console.log(">> func_IsMatchColor IN");
+        if (!src_color_id || !dst_color_id) {
             console.log("\n### Server method_IsMatchColor() ###\n >> error(): it has no src_color");
             resolve("err_undef_color");
         }
-        //1color
-        getColorCategory(src_param.value)
-        .then(function(src_id) {
-            console.log("\n### Server method_IsMatchColor() ###\n >> handling the 1 color case");
-            console.log(" >> src_color info : " + color_pallet[src_id].index + " : " + color_pallet[src_id].code + " : " + color_pallet[src_id].name);
-            res.src_id = src_id;
-            //If cas '2Cloth + 1color', then return source's code
-            if (!dst_param) {resolve(color_pallet[src_id].code);}
-        }).catch(function(err) {
-            console.log(err);
-        })
-        .then(getColorCategory(dst_param.value).then(function(dst_id) {
-            //2color
-            console.log("\n### Server method_IsMatchColor() ###\n >> handling the 2 color case");
-            console.log(" >> target_dst_info : " + dst_param.value);
-            res.dst_id = dst_id;
-          }).catch(function(err) {
-              console.log(err);
-          })
-        )
-        .then(function() {
-            var dst_code = color_pallet[res.dst_id].code;
-            for (i = 0; i < color_combination[res.src_id].length; ++i) {
-                console.log(" >> for_cur_color info : " + color_combination[res.src_id][i]);
-                if (color_combination[res.src_id][i] == dst_code)
+        var dst_code = color_pallet[dst_color_id].code;
+        for (i = 0; i < color_combination[res.src_id].length; ++i) {
+              console.log(" >> for_cur_color info : " + color_combination[res.src_id][i]);
+              if (color_combination[res.src_id][i] == dst_code) {
+                  console.log(">> func_IsMatchColor OUT");
                   resovle("match");
-            }
-            resovle("unmatch");
-        })
+              }
+        }
+        console.log(">> func_IsMatchColor OUT");
+        resovle("unmatch");
     });
 }
-//4.2 컬러 계통찾기
+//4.2 컬러 계통찾기(Find color's index from color_pallet)
 function getColorCategory(color_name) {
     return new Promise(function (resolve, reject) {
-        for (i = 0; i < BASE_COLOR_NUM; ++i) {
-          if (color_name == color_pallet[i].name)
-            resolve(i);
+        console.log(">> func_getColorCategory IN");
+        //Exception : parameter from SK -> abnormal
+        if (!color_name || BASE_COLOR_NUM < 0) {
+          console.log(">> func_getColorCategory IN");
+          reject(new Error("\n>> getColorCategory got error"));
         }
-        reject(new Error("\n>> getColorCategory got error"));
+        //noraml
+        for (i = 0; i < BASE_COLOR_NUM; ++i) {
+          if (color_name == color_pallet[i].name) {
+              console.log(">> func_getColorCategory OUT");
+              resolve(i);
+          }
+        }
     })
 }
-// function getColorCategory(color_name) {
-//     for (i = 0; i < BASE_COLOR_NUM; ++i) {
-//         if (color_name == color_pallet[i].name)
-//             return i;
-//     }
-// }
 //4.$. 서버처리-대기
 app.listen(3000);
 console.log("Listening on port", port);
