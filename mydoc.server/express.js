c4e9be7d0b0ac246b3d8c8ab09d78ca6a878daf7
snrndi121 <snrndi4521@gmail.com>
2019-08-02 14:31:11 +0900

    server : test3

--- mydoc.server/express.js
+++ mydoc.server/express.js
@@ -240,7 +240,7 @@ function IsMatchColor(src_param, dst_param) {
     console.log("\n### Server method_IsMatchColor() ###\n >> handling the 2 color case");
     console.log(" >> target_dst_info : " + dst_param.value);
     for (i = 0; i < color_combination[src_id].length; ++i) {
-        console.log(" >> for_cur_color info : " + color_pallet[src_id][i]);
+        console.log(" >> for_cur_color info : " + color_combination[src_id][i]);
         if (color_combination[src_id][i] == dst_param.value)
           return "match";
     }
